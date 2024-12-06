78f02db0ac1042bd90917b96408de76c54edae2f
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2024-12-06 23:17:56 +0530

    use reducer fixed

--- src/REDUCER/Sample.jsx
+++ src/REDUCER/Sample.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useReducer } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { increament } from './Store';
 import { sampleContext } from '../REDUCER/Context'; // Ensure the path is correct
@@ -10,12 +10,26 @@ const Sample = () => {
 
   console.log(hello, 'hello'); // 'hello' should be printed in console
 
+const initialValue = 1
+const reducer1= (state,action)=>{
+  if (action === 'increment'){
+    return state += 1
+  }else{
+    return state -= 1
+  }
+
+}
+  const [value1, setValue] = useReducer(reducer1,initialValue,)
+
   return (
     <div>
       <h1>hello sample page {count}</h1>
       <button onClick={() => dispatch(increament())}>Increament</button>
       <h2>{hello} to {name} </h2> 
       <button onClick={()=> setName('aju')}>change the name</button>
+
+      <h1>the value{value1}</h1>
+      <button onClick={()=>setValue('increment')}>inc</button>
     </div>
   );
 };
