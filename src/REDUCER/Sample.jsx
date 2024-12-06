fd4bd1e1c1504c758443daaec904fb16a685188d
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2024-12-06 22:29:19 +0530

    redux changed

--- src/REDUCER/Sample.jsx
+++ src/REDUCER/Sample.jsx
@@ -1,30 +1,25 @@
 import React from 'react'
 import {useDispatch, useSelector} from 'react-redux'
-import { increament, setYello } from './Store';
+import { increament } from './Store'
 
 const Sample = () => {
 
-  const sample = useSelector((state)=>state.count.count)
-  console.log(sample,'sample value');
+  const count = useSelector((state)=> state.count.count)
 
   const dispatch = useDispatch()
 
-  const color = useSelector((state)=>state.color.backgroundColor)
-  console.log(color,'clorr');
+
+  
+
   
   
   return (
     <div>
-      <h1>hello sample page</h1>
-      <h1>{sample}</h1>
+      <h1>hello sample page{count}</h1>
 
-      <button onClick={()=>dispatch(increament())}>inc</button>
+      <button onClick={()=>dispatch(increament())}>increament</button>
 
-
-      <div style={{height:'200px',backgroundColor:`${color}`, margin:'10px',width:'200px'}}></div>
-
-      <button onClick={()=>dispatch(setYello())}>change the color</button>
-      
+     
     </div> 
   )
 }
--- src/REDUCER/Store.js
+++ src/REDUCER/Store.js
@@ -1,37 +1,21 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit';
 
-
 const count = createSlice({
   name:'counter',
   initialState:{count:1},
   reducers:{
-    increament:(state)=> {state.count += 1}
+    increament:(state)=>{state.count += 1 }
   }
 })
 
-export const {increament} = count.actions
-
-
-const color = createSlice({
-  name:'color',
-  initialState: {backgroundColor: 'red'},
-  reducers:{
-    setYello: (state)=>{state.backgroundColor = 'yellow'},
-    setGreen: (state)=>{state.backgroundColor = 'green'}
-  }
-})
-
-export const {setGreen,setYello} = color.actions
-
+export const {increament} =count.actions
 
 // Create store
 const store = configureStore({
-  reducer: {
-    count:count.reducer,
-    color: color.reducer
-
-    
-  },
+  reducer:{
+    count:count.reducer
+  }
+  
 });
 
 export default store;
