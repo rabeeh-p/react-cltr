f93a0cc91b00d9df3dede6e27a62ddf1b6438f64
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2024-12-24 23:54:45 +0530

    name added

--- src/components/Counter2.jsx
+++ src/components/Counter2.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 const Counter2 = () => {
     let [count,setCount] =useState(0)
+    let [name,setName] =useState('rabeeh')
 
 
     
@@ -9,17 +10,21 @@ const Counter2 = () => {
         if (count === 0){
             return
         }
-
-
-
-        
         setCount(count - 1)
     }
+
+    const handleChange = ()=>{
+      setName('aju')
+    }
+    
   return (
     <div>
       <h1>{count}</h1>
       <button onClick={()=>setCount(count+1)}>increse</button>
       <button onClick={decrease}>decrease</button>
+
+      <h2>{naem}</h2>
+      <button onClick={handleChange}>change the name</button>
     </div>
   )
 }
